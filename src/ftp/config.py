a46c4a4b9e45019bcbf4d4d46e86e8099e99d43a
bissli <bissli@xyz.com>
2024-04-08 20:07:03 +0000

    unlock Setting by default

--- src/ftp/config.py
+++ src/ftp/config.py
@@ -8,8 +8,6 @@ from libb import Setting, expandabspath
 
 logger = logging.getLogger(__name__)
 
-Setting.unlock()
-
 # Tmpdir
 tmpdir = Setting()
 if os.getenv('CONFIG_TMPDIR_DIR'):
@@ -22,8 +20,6 @@ Path(tmpdir.dir).mkdir(parents=True, exist_ok=True)
 gpg = Setting()
 gpg.dir = os.path.abspath(os.getenv('CONFIG_GPG_DIR') or tmpdir.dir)
 gpg.exe = os.path.join(gpg.dir, 'gpg.exe' if 'Win' in platform.system() else 'gpg')
-
-Setting.lock()
 
 if __name__ == '__main__':
     __import__('doctest').testmod(optionflags=4 | 8 | 32)
