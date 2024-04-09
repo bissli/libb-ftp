ea999eb34a5143f366e41ffecb3972744f25c856
bissli <bissli@xyz.com>
2024-04-09 16:49:35 +0000

    add Setting unlock/lock

--- src/ftp/config.py
+++ src/ftp/config.py
@@ -6,12 +6,16 @@ from libb import Setting, get_tempdir
 
 logger = logging.getLogger(__name__)
 
+Setting.unlock()
+
 tmpdir = get_tempdir()
 
 # GnuPG encryption
 gpg = Setting()
 gpg.dir = os.path.abspath(os.getenv('CONFIG_GPG_DIR') or tmpdir.dir)
 gpg.exe = os.path.join(gpg.dir, 'gpg.exe' if 'Win' in platform.system() else 'gpg')
+
+Setting.lock()
 
 if __name__ == '__main__':
     print('tmpdir: '+tmpdir.dir)
