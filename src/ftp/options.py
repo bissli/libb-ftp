c1137ec18a23451d29bb4532c5a831c45a51b257
bissli <bissli@xyz.com>
2025-10-21 16:57:39 -0400

    added allow_agent and look_for_keys to FtpOptions

--- src/ftp/options.py
+++ src/ftp/options.py
@@ -40,6 +40,8 @@ class FtpOptions(ConfigOptions):
     ssh_key_content: str = None
     ssh_key_passphrase: str = None
     ssh_key_type: str = 'rsa'
+    allow_agent: bool = False
+    look_for_keys: bool = False
 
     # Connection optional
     pgp_extension: str = None
